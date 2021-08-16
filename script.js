24dfb7541dd0e05a642cd5ec6cb29690fca37c7b
reda777 <redazara1998@gmail.com>
2021-08-16 14:04:23 +0100

    added form

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let myLibrary = [];
 const books=document.querySelector('.books');
+const newB=document.querySelector('.newB button:first-child');
+const form=document.querySelector('.newB form');
 function Book(title,author,pages,read) {
     this.title=title;
     this.author=author;
     this.pages=pages;
     this.read=read;
     this.info=function(){
-        return `${title} by ${author}, ${pages} pages, ${(read)?"read":"not read yet"}`;
+        return `${title} 
+        by ${author}, 
+        ${pages} pages, 
+        ${(read)?"read":"not read yet"}`;
     };
 }
 
@@ -24,9 +29,34 @@ function displayBooks(){
     for(let i of myLibrary){
         const book=document.createElement('div');
         book.setAttribute('class','book');
-        const newContent=document.createTextNode(i.info());
-        book.appendChild(newContent);
+        const title=document.createElement('span');
+        const author=document.createElement('span');
+        const pages=document.createElement('span');
+        const status=document.createElement('span');
+        title.textContent='Title :'+i.title;
+        author.textContent='Author :'+i.author;
+        pages.textContent='Pages :'+i.pages;
+        status.textContent=`Status :${(i.read)?"read":"not read yet"}`;
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(status);
         books.appendChild(book);
     }
 }
 displayBooks();
+function showForm(){
+    if(newB.textContent==='add book'){
+        form.style.display='block';
+        newB.textContent='hide form';
+    }
+    else if(newB.textContent==='hide form'){
+        form.style.display='none'; 
+        newB.textContent='add book';
+    }
+}
+newB.addEventListener('click',showForm);
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Saving value", form.elements[0].value);
+});
