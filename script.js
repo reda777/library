b49a4d573273ed086e0eff89d87622a1caff4dd5
reda777 <redazara1998@gmail.com>
2021-08-17 12:24:20 +0100

    new book works

--- script.js
+++ script.js
@@ -15,36 +15,39 @@ function Book(title,author,pages,read) {
     };
 }
 
-function addBookToLibrary(title,author,pages,read) {
+/*function addBookToLibrary(title,author,pages,read) {
     myLibrary.push(new Book(title,author,pages,read));
-}
-addBookToLibrary('overlord','some japanesse guy',1500,true);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-addBookToLibrary('kumo desu','some stupid woman',1200,false);
-function displayBooks(){
+}*/
+
+function displayAllBooks(){
     for(let i of myLibrary){
-        const book=document.createElement('div');
-        book.setAttribute('class','book');
-        const title=document.createElement('span');
-        const author=document.createElement('span');
-        const pages=document.createElement('span');
-        const status=document.createElement('span');
-        title.textContent='Title :'+i.title;
-        author.textContent='Author :'+i.author;
-        pages.textContent='Pages :'+i.pages;
-        status.textContent=`Status :${(i.read)?"read":"not read yet"}`;
-        book.appendChild(title);
-        book.appendChild(author);
-        book.appendChild(pages);
-        book.appendChild(status);
-        books.appendChild(book);
+        displayBook(i);
     }
 }
-displayBooks();
+function displayBook(newBook){
+    const book=document.createElement('div');
+    book.setAttribute('class','book');
+    const title=document.createElement('span');
+    const author=document.createElement('span');
+    const pages=document.createElement('span');
+    const status=document.createElement('span');
+    const remove=document.createElement('span');
+    title.textContent='Title :'+newBook.title;
+    author.textContent='Author :'+newBook.author;
+    pages.textContent='Pages :'+newBook.pages;
+    status.textContent=`Status :${(newBook.read)?"read":"not read yet"}`;
+    remove.textContent='X';
+    remove.setAttribute('class','remove');
+    remove.setAttribute('data-index-number',myLibrary.length-1);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(status);
+    book.appendChild(remove);
+    books.appendChild(book);
+}
+displayAllBooks();
+
 function showForm(){
     if(newB.textContent==='add book'){
         form.style.display='block';
@@ -56,7 +59,21 @@ function showForm(){
     }
 }
 newB.addEventListener('click',showForm);
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("Saving value", form.elements[0].value);
+    console.log(form.elements[0].value,form.elements[1].value
+        ,form.elements[2].value
+        ,(form.elements[3].checked)?form.elements[3].value:form.elements[4].value);
+    myLibrary.push(new Book(form.elements[0].value,form.elements[1].value
+        ,Number(form.elements[2].value)
+        ,(form.elements[3].checked)?true:false));
+    displayBook(myLibrary[myLibrary.length-1]);
+    /*const remove=document.querySelector('.remove');
+    remove.addEventListener('click',removeBook);*/
 });
+/*function removeBook(){
+    console.log(this.dataset.indexNumber);
+    delete myLibrary[this.dataset.indexNumber];
+    displayAllBooks();
+}*/
