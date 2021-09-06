81aa9b51826d66224442c4191751a1bc01042ae1
reda777 <redazara1998@gmail.com>
2021-09-06 17:30:03 +0100

    added dropdown icon, overlay

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const books=document.querySelector('.books');
-const newB=document.querySelector('.newB button:first-child');
-const form=document.querySelector('.newB form');
+const button=document.querySelector('.addBook button');
+const closeAdd=document.querySelector('#closeAdd');
+const form=document.querySelector('.content form');
+const overlay=document.querySelector('.overlay');
 
 function Book(title,author,pages,read) {
     this.title=title;
@@ -9,7 +11,7 @@ function Book(title,author,pages,read) {
     this.read=read;
 }
 function displayBook(bookIndex){
-    bookObj=JSON.parse(localStorage.getItem(bookIndex));
+    let bookObj=JSON.parse(localStorage.getItem(bookIndex));
     /*create book block*/
     const book=document.createElement('div');
     book.setAttribute('id','book'+bookIndex);
@@ -17,6 +19,7 @@ function displayBook(bookIndex){
     /*title div*/
     const titleDiv=document.createElement('div');
     const titleChild1=document.createElement('span');
+    titleChild1.setAttribute('class','child1');
     const titleChild2=document.createElement('span');
     titleChild1.textContent='Title :';
     titleChild2.textContent=bookObj.title;
@@ -25,6 +28,7 @@ function displayBook(bookIndex){
     /*author div*/
     const authorDiv=document.createElement('div');
     const authorChild1=document.createElement('span');
+    authorChild1.setAttribute('class','child1');
     const authorChild2=document.createElement('span');
     authorChild1.textContent='Author :';
     authorChild2.textContent=bookObj.author;
@@ -33,6 +37,7 @@ function displayBook(bookIndex){
     /*pages div*/
     const pagesDiv=document.createElement('div');
     const pagesChild1=document.createElement('span');
+    pagesChild1.setAttribute('class','child1');
     const pagesChild2=document.createElement('span');
     pagesChild1.textContent='Num of pages :';
     pagesChild2.textContent=bookObj.pages;
@@ -41,93 +46,73 @@ function displayBook(bookIndex){
     /*read status*/
     const statusDiv=document.createElement('div');
     const statusChild1=document.createElement('span');
+    statusChild1.setAttribute('class','child1');
     const statusChild2=document.createElement('span');
     const statusChildToggle=document.createElement('div');
     statusChild1.textContent='Have read :';
-    statusChild2.textContent=bookObj.read;
+    statusChild2.textContent=`${(bookObj.read)?'yes':'no'}`;
     statusChildToggle.textContent='Tog';
+    statusChildToggle.addEventListener('click',()=>{
+        (bookObj.read)?bookObj.read=false:bookObj.read=true;
+        statusChild2.textContent=`${(bookObj.read)?'yes':'no'}`;
+        localStorage.setItem(bookIndex,JSON.stringify(bookObj));
+    });
     statusDiv.appendChild(statusChild1);
     statusDiv.appendChild(statusChild2);
     statusDiv.appendChild(statusChildToggle);
-    /*delete book button*/
+    /*edit button*/
+    const dropDownDiv=document.createElement('div');
+    dropDownDiv.setAttribute('class','dropdown');
+    const dropDownButton=document.createElement('span');
+    const dropDownImg=document.createElement('img');
+    dropDownImg.setAttribute('src','images/icon.svg');
+    const dropDownContent=document.createElement('div');
+    dropDownContent.setAttribute('class','dropcontent');
+    const editButton=document.createElement('span');
+    editButton.textContent='Edit';
+    const deleteButton=document.createElement('span');
+    deleteButton.textContent='Delete';
+    dropDownButton.appendChild(dropDownImg);
+    dropDownContent.appendChild(editButton);
+    dropDownContent.appendChild(deleteButton);
+    dropDownDiv.appendChild(dropDownButton);
+    dropDownDiv.appendChild(dropDownContent);
+    dropDownButton.addEventListener('click',()=>{
+        let compStyle=window.getComputedStyle(dropDownContent).display;
+        if(compStyle==='flex')
+            dropDownContent.style.display='none';
+        if(compStyle==='none')
+            dropDownContent.style.display='flex';
+    });
+    deleteButton.addEventListener('click',()=>{
+        let deleteBook=document.querySelector('#book'+bookIndex);
+        deleteBook.remove();
+        localStorage.removeItem(bookIndex);
+    });
+    /*delete book button
     const remove=document.createElement('div');
     remove.textContent='X';
     remove.addEventListener('click',()=>{
         let deleteBook=document.querySelector('#book'+bookIndex);
         deleteBook.remove();
         localStorage.removeItem(bookIndex);
-    });
+    });*/
     /*append children*/
-    book.appendChild(remove);
+    book.appendChild(dropDownDiv);
     book.appendChild(titleDiv);
     book.appendChild(authorDiv);
     book.appendChild(pagesDiv);
     book.appendChild(statusDiv);
     books.appendChild(book);
 }
-/*
-function displayAllBooks(){
-    books.innerHTML='';
-    for(let i in localStorage){
-        if (localStorage.hasOwnProperty(i)) {
-        newBook=JSON.parse(localStorage.getItem(i));
-        const book=document.createElement('div');
-        book.setAttribute('id',i);
-        book.setAttribute('class','book');
-        const title=document.createElement('span');
-        const author=document.createElement('span');
-        const pages=document.createElement('span');
-        const status=document.createElement('span');
-        status.setAttribute('class','status');
-        const toggle=document.createElement('span');
-        const remove=document.createElement('span');
-        title.setAttribute('class','title');
-        title.textContent='Title :'+newBook.title;
-        author.setAttribute('class','author');
-        author.textContent='Author :'+newBook.author;
-        pages.setAttribute('class','pages');
-        pages.textContent='Pages :'+newBook.pages;
-        status.textContent=`Read :${(newBook.read)?'yes':'no'}`;
-        status.setAttribute('data-index-number',i);
-        toggle.textContent='toggle';
-        toggle.setAttribute('class','tog');
-        toggle.addEventListener('click',()=>{
-            (newBook.read)?newBook.read=false:newBook.read=true;
-            let status=document.querySelector('.status[data-index-number="'+i+'"]');
-            status.textContent=`Read :${(newBook.read)?'yes':'no'}`;
-            localStorage.setItem(i,JSON.stringify(newBook));
-        });
-        remove.textContent='X';
-        remove.setAttribute('class','remove');
-        remove.setAttribute('data-index-number',i);
-        remove.addEventListener('click',()=>{
-            let deleteBook=document.querySelector(
-                '[data-index-number="'+i+'"]').parentNode;
-            deleteBook.remove();
-            localStorage.removeItem(i);
-        });
-        book.appendChild(remove);
-        book.appendChild(title);
-        book.appendChild(author);
-        book.appendChild(pages);
-        book.appendChild(status);
-        status.appendChild(toggle);
-        books.appendChild(book);
-    }}
-        
-}
-*/
-newB.addEventListener('click',()=>{
-    if(newB.textContent==='add book'){
-        form.style.display='block';
-        newB.textContent='hide form';
-    }
-    else if(newB.textContent==='hide form'){
-        form.style.display='none'; 
-        newB.textContent='add book';
-    }
+/*hide/show form*/
+button.addEventListener('click',()=>{
+        overlay.style.display = "flex";
 });
-
+closeAdd.addEventListener('click',()=>{
+    overlay.style.display = "none";
+});
+/*submit button*/
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     let insertBook=new Book(form.elements[0].value,form.elements[1].value
@@ -135,6 +120,7 @@ form.addEventListener('submit', function(event) {
         ,(form.elements[3].checked)?true:false);
     localStorage.setItem(localStorage.length,JSON.stringify(insertBook));
     displayBook(localStorage.length-1);
+    overlay.style.display = "none";
 });
 
 window.onload=function(){
@@ -150,3 +136,9 @@ window.onload=function(){
         }  
     }     
 };
+window.addEventListener('click', function(e) {
+    var content=document.querySelector('.dropcontent');
+    if (e.target != document.querySelector('.dropdown>span')) {
+        content.style.display = 'none';
+    }
+});
