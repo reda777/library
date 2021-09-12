010660625dca020b0e91e864b4ac69d90e0fd58e
reda777 <redazara1998@gmail.com>
2021-09-12 14:25:36 +0100

    design tweak

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function displayBook(bookIndex){
     const pagesChild1=document.createElement('span');
     pagesChild1.setAttribute('class','child1');
     const pagesChild2=document.createElement('span');
-    pagesChild1.textContent='Num of pages :';
+    pagesChild1.textContent='N° Pages :';
     pagesChild2.textContent=bookObj.pages;
     pagesDiv.appendChild(pagesChild1);
     pagesDiv.appendChild(pagesChild2);
@@ -52,24 +52,20 @@ function displayBook(bookIndex){
     const statusChild1=document.createElement('span');
     statusChild1.setAttribute('class','child1');
     const statusChild2=document.createElement('span');
-    const statusChildToggle=document.createElement('div');
     statusChild1.textContent='Have read :';
     statusChild2.textContent=`${(bookObj.read)?'yes':'no'}`;
-    statusChildToggle.textContent='Tog';
-    statusChildToggle.addEventListener('click',()=>{
-        (bookObj.read)?bookObj.read=false:bookObj.read=true;
-        statusChild2.textContent=`${(bookObj.read)?'yes':'no'}`;
-        localStorage.setItem(bookIndex,JSON.stringify(bookObj));
-    });
     statusDiv.appendChild(statusChild1);
     statusDiv.appendChild(statusChild2);
-    statusDiv.appendChild(statusChildToggle);
     /*edit button*/
     const dropDownDiv=document.createElement('div');
     dropDownDiv.setAttribute('class','dropdown');
     const dropDownButton=document.createElement('span');
+    dropDownButton.setAttribute('class','dropdown-button');
     const dropDownImg=document.createElement('img');
     dropDownImg.setAttribute('src','images/icon.svg');
+    const saveEdit=document.createElement('span');
+    saveEdit.setAttribute('class','saveButton');
+    saveEdit.textContent='save';
     const dropDownContent=document.createElement('div');
     dropDownContent.setAttribute('class','dropcontent');
     const editButton=document.createElement('span');
@@ -79,8 +75,9 @@ function displayBook(bookIndex){
     dropDownButton.appendChild(dropDownImg);
     dropDownContent.appendChild(editButton);
     dropDownContent.appendChild(deleteButton);
+    dropDownButton.appendChild(dropDownContent);
+    dropDownDiv.appendChild(saveEdit);
     dropDownDiv.appendChild(dropDownButton);
-    dropDownDiv.appendChild(dropDownContent);
     dropDownButton.addEventListener('click',()=>{
         const compStyle=window.getComputedStyle(dropDownContent).display;
         if(compStyle==='flex')
@@ -90,18 +87,58 @@ function displayBook(bookIndex){
     });
     editButton.addEventListener('click',()=>{
         const editBook=document.querySelector('#book'+bookIndex);
+        editBook.querySelector('.dropdown').setAttribute('style','justify-content: space-between;');
+        editBook.querySelector('.saveButton').style.display='block';
         const editChild1=editBook.querySelectorAll('.child1 + span');
         editChild1.forEach(element => {
-            element.textContent='';
-            const inputTitle=document.createElement('input');
-            inputTitle.setAttribute('type','text');
-            element.appendChild(inputTitle);
+            if(element.parentNode.className === 'statusDiv'){
+                const label1=document.createElement('label');
+                const label2=document.createElement('label');
+                const input1=document.createElement('input');
+                const input2=document.createElement('input');
+                label1.textContent='yes';
+                label2.textContent='no';
+                input1.setAttribute('type','radio');
+                input2.setAttribute('type','radio');
+                input1.setAttribute('name','editedStatus'+bookIndex);
+                input2.setAttribute('name','editedStatus'+bookIndex);
+                input1.setAttribute('value','true');
+                input2.setAttribute('value','false');
+                input1.checked=true;
+                element.textContent='';
+                label1.appendChild(input1);
+                label2.appendChild(input2);
+                element.appendChild(label1);
+                element.appendChild(label2);
+                /*element.textContent='Tog';
+                element.addEventListener('click',()=>{
+                    (bookObj.read)?bookObj.read=false:bookObj.read=true;
+                    statusChild2.textContent=`${(bookObj.read)?'yes':'no'}`;
+                    localStorage.setItem(bookIndex,JSON.stringify(bookObj));
+                });*/
+            }
+            else{
+                element.textContent='';
+                const inputTitle=document.createElement('input');
+                inputTitle.setAttribute('type','text');
+                element.appendChild(inputTitle);
+            }
         });
     });
     deleteButton.addEventListener('click',()=>{
         const deleteBook=document.querySelector('#book'+bookIndex);
         deleteBook.remove();
         localStorage.removeItem(bookIndex);
+    });
+    saveEdit.addEventListener('click',()=>{
+        const saveBook=document.querySelector('#book'+bookIndex);
+        let insertBook=new Book(saveBook.getElementsByTagName("input")[0].value,saveBook.getElementsByTagName("input")[1].value
+            ,Number(saveBook.getElementsByTagName("input")[2].value)
+            ,(saveBook.getElementsByTagName("input")[3].checked)?true:false);
+        localStorage.setItem(bookIndex,JSON.stringify(insertBook));
+        books.innerHTML='';
+        displayAllBooks();
+        updateStats();
     });
     /*append children*/
     book.appendChild(dropDownDiv);
@@ -113,10 +150,29 @@ function displayBook(bookIndex){
 }
 /*show stats*/
 function updateStats(){
+    const liselect=document.querySelectorAll('.stats ul li')
+    if(liselect){
+        liselect.forEach(element =>{
+            element.remove();
+        });
+    }
     const stats=document.querySelector('.stats ul');
+    /*to read*/
     const li1=document.createElement('li');
-    li1.textContent=`To read (${ localStorageFullLength()-readBooksCount()})`;
+    li1.textContent=`To read (${ localStorage.length-readBooksCount()})`;
     stats.appendChild(li1);
+    /*have read*/
+    const li2=document.createElement('li');
+    li2.textContent=`Have read (${readBooksCount()})`;
+    stats.appendChild(li2);
+    /*reading now*/
+    const li3=document.createElement('li');
+    li3.textContent=`Reading now (${0})`;
+    stats.appendChild(li3);
+    /*favorites*/
+    const li4=document.createElement('li');
+    li4.textContent=`Favorites (${0})`;
+    stats.appendChild(li4);
 }
 /*hide/show form*/
 button.addEventListener('click',()=>{
@@ -165,7 +221,7 @@ function localStorageFullLength(){
     }
     return i;  
 };
-window.onload=function(){
+function displayAllBooks(){
     let i=0;
     let count=0;
     while(count<localStorage.length){
@@ -177,6 +233,10 @@ window.onload=function(){
             count++;
         }  
     }     
+};
+window.onload=function(){
+    displayAllBooks();
+    updateStats();
 };
 window.addEventListener('click', function(e) {
     const allBooks=document.querySelectorAll('.book');
